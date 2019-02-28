1877c9d0290ef755794d9e5546ed7358af682c83
brindab50 behal <brindab50@gmail.com>
2019-02-28 19:22:08 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,3 +1,11 @@
-def fizzbuzz(3)
-  return "Fizz"
+def fizzbuzz(num)
+  if (num == 3)
+    return "Fizz"
+  elsif (num == 5)
+   return "Buzz"
+  elsif (num == 15)
+   return "FizzBuzz"
+  elsif (num == 4)
+   return nil
+  end
 end
